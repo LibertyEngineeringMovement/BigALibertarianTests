f46497df616976b2d86bac19de64064b88229fbd
Brian Webb <BrianProgrammer@outlook.com>
2015-03-17 22:14:21 -0700

    Started the Editor
    Mostly layout and link edits.

--- app/app.js
+++ app/app.js
@@ -9,9 +9,6 @@
 
     app.config(['$routeProvider', buildRoute]);
 
-    //app.controller('listCtrl', ['$http', buildListController]);
-    //app.controller('testCtrl', ['$http', '$routeParams', buildTestController]);
-
     function buildRoute($routeProvider) {
 
         $routeProvider.
@@ -25,137 +22,13 @@
               controller: 'testCtrl',
               controllerAs: 'ctrl'
           }).
+          when('/build', {
+              templateUrl: 'app/test/testBuild.html',
+              controller: 'testBuildCtrl',
+              controllerAs: 'ctrl'
+          }).
           otherwise({
               redirectTo: '/lists'
           });
     }
-
-    //function buildListController($http) {
-    //    var me = this;
-
-    //    initialize();
-
-    //    function initialize() {
-
-    //        toastr.info("Loading Test Catalog");
-
-    //        $http.get('TestRepository/TestIndex.json')
-    //            .success(function (data, status, headers, config) {
-    //                toastr.success('Got Data', status);
-    //                me.tests = data;
-    //            })
-    //            .error(function (data, status, headers, config) {
-    //                toastr.error("Didn't get data.", data);
-    //                me.tests = [];
-    //            })
-    //    }
-    //}
-
-    //function buildTestController($http, $routeParams) {
-    //    var me = this;
-    //    me.selection = '';
-    //    me.currentQuestionIdx = 0;
-    //    me.questionCount = 0;
-    //    me.progressPercent = 100;
-    //    me.showAssumptions = true;
-    //    me.questionText = '';
-    //    me.buttons = [];
-    //    me.score = 0;
-    //    me.summaryVisible = false;
-    //    me.currentQuestionObj = {};
-    //    me.btnComment = '';
-
-    //    me.selectAnswer = selectAnswer;
-    //    me.nextQuestion = nextQuestion;
-    //    me.toggleAssumptions = toggleAssumptions;
-    //    me.score = [];
-    //    me.scoreTotal = 0;
-    //    me.resultMessage = '';
-        
-
-    //    initialize();
-
-    //    function initialize() {
-
-    //        toastr.info("Loading the Test");
-
-    //        me.selection = $routeParams.testId;
-
-    //        $http.get('TestRepository/Test' + ('000' + me.selection.toString()).right(3) + '.json')
-    //            .success(function (data, status, headers, config) {
-    //                toastr.success('Test Loaded');
-    //                prepareTestPage(data);
-    //            })
-    //            .error(function (data, status, headers, config) {
-    //                toastr.error("Didn't get test.");
-    //                me.test = [];
-    //            })
-
-    //    }
-
-    //    function prepareTestPage(data) {
-    //        me.test = data;
-    //        me.questionCount = me.test.questions.length;
-
-    //        if (me.questionCount === 0) { //we are done.
-    //            toastr.warning('There are no questions.');
-    //            return;
-    //        } 
-
-    //        me.progressPercent = (me.currentQuestionIdx / me.questionCount) * 100;
-
-    //        loadQuestion();
-    //    }
-
-    //    function toggleAssumptions() {
-    //        me.showAssumptions = !me.showAssumptions;
-    //    }
-
-    //    function selectAnswer(button) {
-
-    //        for (var i = 0; i < me.buttons.length; i++) {
-    //            me.buttons[i].selected = false;
-    //        }
-    //        button.selected = true;
-    //        me.score[me.currentQuestionIdx] = button.value;
-    //        me.scoreTotal = Math.sum(me.score);
-
-    //        me.btnComment = button.selectedComment;
-
-    //    }
-
-    //    function nextQuestion() {
-    //        me.currentQuestionIdx = me.currentQuestionIdx + 1;
-
-    //        loadQuestion();
-    //    }
-
-    //    function loadQuestion() {
-    //        if (me.currentQuestionIdx < me.questionCount) {
-    //            me.progressPercent = (me.currentQuestionIdx / me.questionCount) * 100;
-
-    //            me.currentQuestionObj = me.test.questions[me.currentQuestionIdx];
-
-    //            me.questionText
-    //                = me.currentQuestionObj.question;
-    //            me.buttons
-    //                = me.currentQuestionObj.buttons;
-
-    //            me.btnComment = '';
-    //        }
-    //        else {
-    //            me.progressPercent = 100;
-    //            me.summaryVisible = true;
-
-    //            var msg = me.test.results.find(
-    //                function (row){ 
-    //                    return row.score === me.scoreTotal; 
-    //                }).caption;
-
-    //            me.resultMessage = msg || '';
-
-    //        }
-    //    }
-    //}
-
 })()
---
+++ app/testBuildController.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../Scripts/_ref.js" />
+(function () {
+    'use strict';
+    var app = angular.module('app');
+    app.controller('testBuildCtrl', ['$http', buildTestBuildController]);
+    function buildTestBuildController($http) {
+        var me = this;
+        initialize();
+        function initialize() {
+        }
+    }
+})();
