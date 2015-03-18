4a241d48cdc40191425079b234832ca19943c277
Brian Webb <BrianProgrammer@outlook.com>
2015-03-17 23:37:02 -0700

    Added upload to browser
    You can now upload a json file into the browser, and it parses to an
    object.
    Next stunt are the root fields.

--- app/testBuildController.js
+++ app/testBuildController.js
@@ -2,11 +2,51 @@
 (function () {
     'use strict';
     var app = angular.module('app');
+
     app.controller('testBuildCtrl', ['$http', buildTestBuildController]);
+    app.directive('inputFile', buildFileHandlerDirective);
+
     function buildTestBuildController($http) {
         var me = this;
+        me.test = {};
+
         initialize();
         function initialize() {
         }
     }
+
+    function buildFileHandlerDirective() {
+        return {
+            scope: {
+                fileModel: "="
+            },
+            restrict: 'E',
+            link: function (scope, element, attributes) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+                    var rawData = {};
+
+                    //reader.onabort = function (eventArg) { console.log("ABORT!", eventArg); };
+                    //reader.onerror = function (eventArg) { console.log("ERROR!", eventArg); };
+                    //reader.onloadstart = function (eventArg) { console.log("START!", eventArg); };
+                    //reader.onloadend = function (eventArg) { console.log("END!", eventArg); };
+                    //reader.onprogress = function (eventArg) { console.log("PROGRESS!", eventArg); };
+
+                    reader.onload = function (loadEvent) {
+                        scope.$apply(function () {
+                            rawData = loadEvent.target.result;
+                            scope.fileModel = JSON.parse(rawData);
+                            //console.log("READ!", rawData);
+                        });
+                        //console.log("LOAD!", loadEvent);
+                    }
+                    reader.readAsText(changeEvent.target.files[0]);
+                    //console.log("FILES!", changeEvent.target.files);
+                });
+            },
+            template: '<input type="file" class="form-control" accept="application/json" />'
+        }
+    }
+
 })();
+
